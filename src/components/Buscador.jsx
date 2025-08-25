cc76c3758074e4ab71ab2ff96c7af006bbbac863
Santiago Robledo <santiddf9@gmail.com>
2025-08-25 02:47:17 -0300

    agregue un buscador

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,31 +1,25 @@
+import SeccionNoticia from "./SeccionNoticia";
+
 const Buscador = () => {
   return (
-    <div className="border-primary my-10 text-center mx-2">
-      <h1 className="font-bold text-4xl">Buscar noticias</h1>
-      <div>
-        <form className="mt-2">
-          <label className="input border-none vidrio focus:outline-none">
-            <svg
-              className="h-[1em] opacity-50"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <g
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2.5"
-                fill="none"
-                stroke="currentColor"
-              >
-                <circle cx="11" cy="11" r="8"></circle>
-                <path d="m21 21-4.3-4.3"></path>
-              </g>
-            </svg>
-            <input type="search" required placeholder="Search" />
-          </label>
-        </form>
+    <>
+      <div className="border-primary my-10 text-center mx-2">
+        <h1 className="font-bold text-4xl">Buscar noticias</h1>
+        <div>
+          <form className="mt-2">
+            <select id="estado" name="estado" className="select vidrio" required>
+              <option value="" className="">Selecciona una categoria</option>
+              <option value="creada">Creada</option>
+              <option value="en_proceso">En proceso</option>
+              <option value="terminada">Terminada</option>
+            </select>
+          </form>
+        </div>
       </div>
-    </div>
+      <div>
+        <SeccionNoticia />
+      </div>
+    </>
   );
 };
 
---
+++ src/components/SeccionNoticia.jsx
@@ -0,0 +1,9 @@
+const SeccionNoticia = () => {
+    return (
+      <div className="border mx-10 glass">
+        <h3>Seccion Noticia</h3>
+      </div>
+    );
+};
+
+export default SeccionNoticia;
