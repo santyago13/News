090fa3e7747627777e4cd76439a0e55f6c67da2f
Santiago Robledo <santiddf9@gmail.com>
2025-08-25 03:22:58 -0300

    corregi la seccion de articulos

--- src/components/SeccionNoticia.jsx
+++ src/components/SeccionNoticia.jsx
@@ -2,13 +2,15 @@ import CardNoticia from "./CardNoticia";
 
 const SeccionNoticia = () => {
   return (
-    <div className="mb-10 vidrio mx-15 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6 p-6 place-items-center">
-      <CardNoticia />
-      <CardNoticia />
-      <CardNoticia />
-      <CardNoticia />
-      <CardNoticia />
-    </div>
+    <section>
+      <div className="mb-10 vidrio mx-15 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6 p-6 place-items-center">
+        <CardNoticia />
+        <CardNoticia />
+        <CardNoticia />
+        <CardNoticia />
+        <CardNoticia />
+      </div>
+    </section>
   );
 };
 
